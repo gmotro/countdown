7110b28aad817624c6e5576159f63c3ca2541c44
GMotro <motrogena@yahoo.com>
2015-01-24 14:56:16 -0700

    Completed JS CountDown task
    Completed JS CountDown task

--- js/countdown.js
+++ js/countdown.js
@@ -1,19 +1,49 @@
-// Function to complete.
-function CountDownTimer(dateTime, divId) {
-    var end     = new Date(dateTime);
-    var _second = 1000;
-    var _minute = _second * 60;
-    var _hour   = _minute * 60;
-    var _day    = _hour * 24;
-    var timer;
+function CountDownTimer( dateTime, divId) {
+	
+           //var end     = new Date(dateTime);
+		    var _second = 1000;
+			var _minute = _second * 60;
+			var _hour   = _minute * 60;
+			var _day    = _hour * 24;
+			var timer;
+		
+		setInterval(function () {
 
-    function showRemaining() {
-       // ... 
-    }
+		    timer = new Date(dateTime).getTime() - new Date().getTime(); 
+								 
+			_days = parseInt(timer / _day);
+			timer = timer % _day;
+			 			 
+			_hours = parseInt(timer / _hour);
+			timer = timer % _hour;			
+			 
+			_minutes = parseInt(timer / _minute);
+			timer = timer % _minute;
+			
+			_seconds = parseInt(timer / _second);
+			 
+			document.getElementById(divId).innerHTML = "GroundHog's Day will be in: <br>"+_days + "d, " + _hours + "h, " + _minutes + "m, " + _seconds + "s"; 
+		
+		},1000);
+		
+		/*function showRemaining() {	
 
-    timer = setInterval(showRemaining(), 1000);
-}
-
-// Enter the date and time as a string in the following format.  Second parameter is the HTML id of element where you want
-//    the countdown to appear.
-CountDownTimer('02/02/2015 12:00 AM', 'countDown');
+		    timer = new Date(dateTime).getTime() - new Date().getTime(); 
+								 
+			_days = parseInt(timer / _day);
+			timer = timer % _day;
+			 			 
+			_hours = parseInt(timer / _hour);
+			timer = timer % _hour;			
+			 
+			_minutes = parseInt(timer / _minute);
+			timer = timer % _minute;
+			
+			_seconds = parseInt(timer / _second);
+			 
+			document.getElementById(divId).innerHTML = "GroundHog's Day will be in: <br>"+_days + "d, " + _hours + "h, " + _minutes + "m, " + _seconds + "s"; 
+		}		
+		setInterval(showRemaining() , 1000);*/	
+	}
+	
+CountDownTimer('02/02/2015 12:00 AM', 'countDown');	
